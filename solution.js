ff9f568169fa2a097524ca032ef02acad9503ddb
Bastian Dammann <dammann.bastian@gmail.com>
2023-03-23 16:25:23 +0100

    added a simulated transaction to check whether there is appropriate change in the cashbox

--- solution.js
+++ solution.js
@@ -1,4 +1,5 @@
 const createCashCounter = () => {
+  // creating a variable that holds an array of objects with the different banknotes and coins. I used cent values because JS has a hard time dealing with decimal numbers.
   let cashBox = [
     { 5000: 10 },
     { 2000: 10 },
@@ -14,17 +15,17 @@ const createCashCounter = () => {
     { 1: 25 },
   ];
 
-  // Calculate Cashbox Total before any transactions:
+  // A function to calculate the total amount of money in the cashbox. I use this function in a console.log whenever the cashCounter function is being called
   const cashBoxNet = () => {
     return cashBox.reduce((acc, curr, i) => {
       for (const amount in curr) {
         acc += amount * curr[amount];
-    }
+      }
       return acc;
     }, 0);
   };
 
-  // returning the function for calculating the most efficient change
+  // returning the actual cashCounter function for calculating the most efficient way of giving back change for a transaction
   return (price, paid) => {
     console.log("---TRANSACTION START---");
     console.log(
@@ -43,58 +44,108 @@ const createCashCounter = () => {
       return `You payed appropriately, thanks man!`;
       // if price is higher than paid, request more money
     } else if (paid < price) {
-      return `Your payment is ${((price - paid) / 100).toFixed(2)}€ short.`;
-      // if paid is higher than price, add paid to cashbox and give adequate change
-    } else if (change > cashBoxNet()) {
-      return "Not enough change, sorry."
+      return `Sorry, but your payment is ${((price - paid) / 100).toFixed(
+        2
+      )}€ short.`;
+
+      /* if paid is higher than price:
+      - simulate the following: 
+        1. Create new variables, so that originals dont change during simulation:
+        2. Loop over the cashbox
+        3. loop over each object
+        4. and while the banknote (eg. 50€, 20€) is smaller than the paid amount, ill add the banknote to the cashbox and subtract it from the money paid. If its not, check next banknote and so on... until all of the paid money is in the cashbox.
+        This way, the money paid is always paid in the most efficient way. (eg. paid = 50€ will never be paid with [20€, 20€, 10€] but with [50€])
+      - then the simulation does the same, but reverse for the change
+        1. Loop over cashbox
+        2. loop over each object
+        3. and while the banknote is smaller than the change, the banknote will be subtracted from the cashbox and at the same time it will be pushed to the changeArr. 
+        If its not, check next bank note and so on until all the change has been paid back.
+        This way, the change is always given back in the most efficient way. (eg. change = 50€ will never be returned with [20€, 20€, 10€] but with [50€])
+      - and finally I decided to check the length of the changeArr to determine if it is worthy of accepting the transaction. If the changeArr.length is higher than 12 (who wants to receive 12 coins for change?? ;) ), the transaction will be declined, otherwise the exact same code will be run again, but this time not as a simulation, but with the real cashBox.
+        */
     } else if (paid > price) {
-      // ! Adding paid to cashBox
-  
-      cashBox.forEach((value) => {
+      // ! Simulate adding paid to cashbox and then checking whether there is enough and appropriate change in the cashbox. If not, do not accept the transaction, else do.
+      let cashBoxSim = JSON.parse(JSON.stringify(cashBox));
+      let changeSim = change;
+      let paidSim = paid;
+      cashBoxSim.forEach((value) => {
         for (const amount in value) {
-          while (paid >= amount) {
-            paid -= amount;
+          while (paidSim >= amount) {
+            paidSim -= amount;
             value[amount]++;
           }
         }
       });
-      // !
-      // ! Subtracting always highest banknote from change until change is 0 and remove those banknotes from cashbox
-      console.log("This is the Change in €:", (change / 100).toFixed(2));
-      const changeArr = [];
-      cashBox.forEach((value) => {
+
+      const changeArrSim = [];
+      cashBoxSim.forEach((value) => {
         for (const amount in value) {
-          while (change >= amount && value[amount] > 0) {
-            change -= amount;
+          while (changeSim >= amount && value[amount] > 0) {
+            changeSim -= amount;
             value[amount]--;
-            changeArr.push(amount);
+            changeArrSim.push(amount);
           }
         }
       });
-      console.log("The most efficient way to hand out the change:", changeArr);
+      //! Simulation End
+      if (changeArrSim.length > 12) {
+        return "Sorry mate, I can't give you appropriate change without emptying my coin pockets into your wallet.";
+      } else {
+        cashBox.forEach((value) => {
+          for (const amount in value) {
+            while (paid >= amount) {
+              paid -= amount;
+              // ! Actually adding paid to original cashBox
+
+              value[amount]++;
+            }
+          }
+        });
+
+        console.log("This is the Change in €:", (change / 100).toFixed(2));
+        const changeArr = [];
+        cashBox.forEach((value) => {
+          for (const amount in value) {
+            // ! Subtracting always highest banknote from change until change is 0 and remove those banknotes from cashbox
+            while (change >= amount && value[amount] > 0) {
+              change -= amount;
+              value[amount]--;
+              //! Pushing the banknotes and coins to the changeArr array, which then holds the total amount of banknotes and coins for the change
+              changeArr.push(amount);
+            }
+          }
+        });
+        console.log(
+          "The most efficient way to hand out the change:",
+          changeArr
+        );
       }
-    console.log("Updated Cashbox after transaction:");
-    return cashBox;
+      console.log("Updated Cashbox after transaction:");
+      return cashBox;
+    }
   };
 };
 
 const cashCounter = createCashCounter();
 
-console.log(cashCounter(3250, 5000));
-console.log(cashCounter(3250, 5000));
-console.log(cashCounter(3250, 5000));
-console.log(cashCounter(3250, 5000));
-console.log(cashCounter(3250, 5000));
-console.log(cashCounter(3250, 5000));
-console.log(cashCounter(3250, 5000));
-console.log(cashCounter(3250, 5000));
-console.log(cashCounter(3250, 5000));
-console.log(cashCounter(3250, 5000));
-console.log(cashCounter(3250, 5000));
-console.log(cashCounter(3250, 5000));
-console.log(cashCounter(3250, 5000));
-console.log(cashCounter(3250, 5000));
-console.log(cashCounter(3250, 5000));
-console.log(cashCounter(3250, 5000));
-console.log(cashCounter(3250, 5000));
-console.log(cashCounter(3250, 5000));
+console.log(cashCounter(3750, 5000));
+console.log(cashCounter(2350, 5000));
+console.log(cashCounter(2350, 5000));
+console.log(cashCounter(2350, 5000));
+console.log(cashCounter(2350, 5000));
+console.log(cashCounter(2350, 5000));
+console.log(cashCounter(2350, 5000));
+console.log(cashCounter(2350, 5000));
+console.log(cashCounter(2350, 5000));
+console.log(cashCounter(2350, 5000));
+console.log(cashCounter(2350, 5000));
+console.log(cashCounter(2350, 5000));
+console.log(cashCounter(2350, 5000));
+console.log(cashCounter(2350, 5000));
+console.log(cashCounter(2350, 5000));
+console.log(cashCounter(2350, 5000));
+console.log(cashCounter(2350, 5000));
+console.log(cashCounter(2350, 5000));
+console.log(cashCounter(2350, 5000));
+console.log(cashCounter(5500, 5000));
+
